263db586f5fd673582b90f0f70141104ea83e966
sandrosa1 <sandro.sa@fatec.sp.gov.br>
2021-03-23 17:48:46 -0300

    recolhe teste

--- paginaTeste.php
+++
@@ -1,9 +0,0 @@
-<?php include 'include/nav.php'; ?>
-<div class="container">
-    <div class="row">
-        <div class="col-md-12 titulo">
-            <h3>Teste Docker</h3>
-        </div>
-    </div>
-</div>
-<?php include 'include/bottom.php'; ?>
